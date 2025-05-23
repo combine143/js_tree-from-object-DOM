6553eabd85e83884b60a62443480a09bc9cc130a
combine143 <combine142@gmail.com>
2025-05-23 13:22:21 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,31 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  let treeElement = element;
+
+  if (!treeElement) {
+    treeElement = document.createElement('div');
+    treeElement.id = 'tree';
+    document.body.appendChild(treeElement);
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (Object.keys(data[key]).length > 0) {
+      createTree(li, data[key]);
+    }
+
+    ul.appendChild(li);
+  }
+
+  treeElement.appendChild(ul);
+
+  return treeElement;
 }
 
 createTree(tree, food);
