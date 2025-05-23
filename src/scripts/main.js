e1bd95b0cfd5f8b53bfd15d75a7851d11c3d3865
combine143 <combine142@gmail.com>
2025-05-23 13:53:03 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -29,21 +29,20 @@ function createTree(element, data) {
     document.body.appendChild(treeElement);
   }
 
-  const ul = document.createElement('ul');
-
   for (const key in data) {
     const li = document.createElement('li');
 
     li.textContent = key;
 
     if (Object.keys(data[key]).length > 0) {
-      createTree(li, data[key]);
+      const ul = document.createElement('ul');
+
+      createTree(ul, data[key]);
+      li.appendChild(ul);
     }
 
-    ul.appendChild(li);
+    treeElement.appendChild(li);
   }
-
-  treeElement.appendChild(ul);
 
   return treeElement;
 }
